a4f5719dc4fc7b247dfa4e916c79d91affd8976c
Leonardolmln <leonardolmln@hotmail.com>
2017-01-06 18:12:03 -0200

    DAOs: 24/25

---
+++ NobelTheater/DAOs/FornecedorDAO.vb
@@ -0,0 +1,204 @@
+﻿Option Explicit On
+Option Strict On
+
+Imports System.Text
+
+Public Class FornecedorDAO
+
+    'OK
+    Public Function Insert(ByVal fornecedor As Fornecedor) As Boolean
+        Dim conn As New Connection
+        Dim strSQL As New StringBuilder
+
+        strSQL.Append("INSERT INTO Fornecedores(cnpj, razaoSocial, nomeFantasia, telefone, inicioRelacionamento, tipoFornecedor, logradouro, numero, bairro, cidade, estado, cep, tipoEndereco, cnpjAssistencia) ")
+        strSQL.Append("VALUES(@cnpj, @razaoSocial, @nomeFantasia, @telefone, inicioRelacionamento, @tipoFornecedor, @logradouro, @numero, @bairro, @cidade, @estado, @cep, @tipoEndereco, @cnpjAssistencia);")
+
+        conn.AddParameter("@cnpj", fornecedor.CNPJ)
+        conn.AddParameter("@razaoSocial", fornecedor.RazaoSocial)
+        conn.AddParameter("@nomeFantasia", fornecedor.NomeFantasia)
+        conn.AddParameter("@telefone", fornecedor.Telefone)
+        conn.AddParameter("@inicioRelacionamento", fornecedor.InicioRelacionamento)
+        conn.AddParameter("@tipoFornecedor", fornecedor.TipoFornecedor)
+        conn.AddParameter("@logradouro", fornecedor.Logradouro)
+        conn.AddParameter("@numero", fornecedor.Numero)
+        conn.AddParameter("@bairro", fornecedor.Bairro)
+        conn.AddParameter("@cidade", fornecedor.Cidade)
+        conn.AddParameter("@estado", fornecedor.Estado)
+        conn.AddParameter("@cep", fornecedor.CEP)
+        conn.AddParameter("@tipoEndereco", fornecedor.TipoEndereco)
+        conn.AddParameter("@cnpjAssistencia", fornecedor.Assistencia.CNPJ)
+
+        Return conn.ExecuteCommand(strSQL.ToString)
+
+    End Function
+
+    'OK
+    Public Function Update(ByVal fornecedor As Fornecedor) As Boolean
+        Dim conn As New Connection
+        Dim strSQL As New StringBuilder
+
+        strSQL.Append("UPDATE Fornecedores ")
+        strSQL.Append("SET razaoSocial = @razaoSocial, ")
+        strSQL.Append("nomeFantasia = @nomeFantasia, ")
+        strSQL.Append("telefone = @telefone, ")
+        strSQL.Append("inicioRelacionamento = @inicioRelacionamento, ")
+        strSQL.Append("tipoFornecedor = @tipoFornecedor, ")
+        strSQL.Append("logradouro = @logradouro, ")
+        strSQL.Append("numero = @numero, ")
+        strSQL.Append("bairro = @bairro, ")
+        strSQL.Append("cidade = @cidade, ")
+        strSQL.Append("estado = @estado, ")
+        strSQL.Append("cep = @cep, ")
+        strSQL.Append("tipoEndereco = @tipoEndereco ")
+        strSQL.Append("cnpjAssistencia = @cnpjAssistencia ")
+        strSQL.Append("WHERE cnpj = @cnpj;")
+
+        conn.AddParameter("@razaoSocial", fornecedor.RazaoSocial)
+        conn.AddParameter("@nomeFantasia", fornecedor.NomeFantasia)
+        conn.AddParameter("@telefone", fornecedor.Telefone)
+        conn.AddParameter("@inicioRelacionamento", fornecedor.InicioRelacionamento)
+        conn.AddParameter("@tipoFornecedor", fornecedor.TipoFornecedor)
+        conn.AddParameter("@logradouro", fornecedor.Logradouro)
+        conn.AddParameter("@numero", fornecedor.Numero)
+        conn.AddParameter("@bairro", fornecedor.Bairro)
+        conn.AddParameter("@cidade", fornecedor.Cidade)
+        conn.AddParameter("@estado", fornecedor.Estado)
+        conn.AddParameter("@cep", fornecedor.CEP)
+        conn.AddParameter("@tipoEndereco", fornecedor.TipoEndereco)
+        conn.AddParameter("@cnpjAssistencia", fornecedor.Assistencia.CNPJ)
+        conn.AddParameter("@cnpj", fornecedor.CNPJ)
+
+        Return conn.ExecuteCommand(strSQL.ToString)
+
+    End Function
+
+    'OK
+    Public Function FindByCNPJ(cnpj As String) As Fornecedor
+        Dim conn As New Connection
+        Dim strSQL As New StringBuilder
+
+        strSQL.Append("SELECT * FROM Fornecedores ")
+        strSQL.Append("WHERE cnpj = @cnpj;")
+
+        conn.AddParameter("@cnpj", cnpj)
+
+        Dim dt As DataTable = conn.ExecuteSelect(strSQL.ToString)
+
+        Dim fornecedor As New Fornecedor()
+        fornecedor.CNPJ = CStr(dt.Rows(0).Item("cnpj"))
+        fornecedor.RazaoSocial = CStr(dt.Rows(0).Item("razaoSocial"))
+        fornecedor.NomeFantasia = CStr(dt.Rows(0).Item("nomeFantasia"))
+        fornecedor.Telefone = CStr(dt.Rows(0).Item("telefone"))
+        fornecedor.InicioRelacionamento = CDate(dt.Rows(0).Item("inicioRelacionamento"))
+        fornecedor.TipoFornecedor = CStr(dt.Rows(0).Item("tipoFornecedor"))
+        fornecedor.Logradouro = CStr(dt.Rows(0).Item("logradouro"))
+        fornecedor.Numero = CStr(dt.Rows(0).Item("numero"))
+        fornecedor.Bairro = CStr(dt.Rows(0).Item("bairro"))
+        fornecedor.Cidade = CStr(dt.Rows(0).Item("cidade"))
+        fornecedor.Estado = CStr(dt.Rows(0).Item("estado"))
+        fornecedor.CEP = CStr(dt.Rows(0).Item("cep"))
+        fornecedor.TipoEndereco = CStr(dt.Rows(0).Item("tipoEndereco"))
+        fornecedor.Assistencia = FindAssistenciaByCNPJ(CStr(dt.Rows(0).Item("cnpjAssistencia")))
+
+        Return fornecedor
+
+    End Function
+
+    'OK
+    Public Function FindByRazaoSocial(razaoSocial As String) As Fornecedor
+        Dim conn As New Connection
+        Dim strSQL As New StringBuilder
+
+        strSQL.Append("SELECT * FROM Fornecedores ")
+        strSQL.Append("WHERE razaoSocial = @razaoSocial;")
+
+        conn.AddParameter("@razaoSocial", razaoSocial)
+
+        Dim dt As DataTable = conn.ExecuteSelect(strSQL.ToString)
+
+        Dim fornecedor As New Fornecedor()
+        fornecedor.CNPJ = CStr(dt.Rows(0).Item("cnpj"))
+        fornecedor.RazaoSocial = CStr(dt.Rows(0).Item("razaoSocial"))
+        fornecedor.NomeFantasia = CStr(dt.Rows(0).Item("nomeFantasia"))
+        fornecedor.Telefone = CStr(dt.Rows(0).Item("telefone"))
+        fornecedor.InicioRelacionamento = CDate(dt.Rows(0).Item("inicioRelacionamento"))
+        fornecedor.TipoFornecedor = CStr(dt.Rows(0).Item("tipoFornecedor"))
+        fornecedor.Logradouro = CStr(dt.Rows(0).Item("logradouro"))
+        fornecedor.Numero = CStr(dt.Rows(0).Item("numero"))
+        fornecedor.Bairro = CStr(dt.Rows(0).Item("bairro"))
+        fornecedor.Cidade = CStr(dt.Rows(0).Item("cidade"))
+        fornecedor.Estado = CStr(dt.Rows(0).Item("estado"))
+        fornecedor.CEP = CStr(dt.Rows(0).Item("cep"))
+        fornecedor.TipoEndereco = CStr(dt.Rows(0).Item("tipoEndereco"))
+        fornecedor.Assistencia = FindAssistenciaByCNPJ(CStr(dt.Rows(0).Item("cnpjAssistencia")))
+
+        Return fornecedor
+
+    End Function
+
+    'OK
+    Public Function FindByNomeFantasia(nomeFantasia As String) As Fornecedor
+        Dim conn As New Connection
+        Dim strSQL As New StringBuilder
+
+        strSQL.Append("SELECT * FROM Fornecedores ")
+        strSQL.Append("WHERE nomeFantasia = @nomeFantasia;")
+
+        conn.AddParameter("@nomeFantasia", nomeFantasia)
+
+        Dim dt As DataTable = conn.ExecuteSelect(strSQL.ToString)
+
+        Dim fornecedor As New Fornecedor()
+        fornecedor.CNPJ = CStr(dt.Rows(0).Item("cnpj"))
+        fornecedor.RazaoSocial = CStr(dt.Rows(0).Item("razaoSocial"))
+        fornecedor.NomeFantasia = CStr(dt.Rows(0).Item("nomeFantasia"))
+        fornecedor.Telefone = CStr(dt.Rows(0).Item("telefone"))
+        fornecedor.InicioRelacionamento = CDate(dt.Rows(0).Item("inicioRelacionamento"))
+        fornecedor.TipoFornecedor = CStr(dt.Rows(0).Item("tipoFornecedor"))
+        fornecedor.Logradouro = CStr(dt.Rows(0).Item("logradouro"))
+        fornecedor.Numero = CStr(dt.Rows(0).Item("numero"))
+        fornecedor.Bairro = CStr(dt.Rows(0).Item("bairro"))
+        fornecedor.Cidade = CStr(dt.Rows(0).Item("cidade"))
+        fornecedor.Estado = CStr(dt.Rows(0).Item("estado"))
+        fornecedor.CEP = CStr(dt.Rows(0).Item("cep"))
+        fornecedor.TipoEndereco = CStr(dt.Rows(0).Item("tipoEndereco"))
+        fornecedor.Assistencia = FindAssistenciaByCNPJ(CStr(dt.Rows(0).Item("cnpjAssistencia")))
+
+        Return fornecedor
+
+    End Function
+
+    'OK
+    Public Function FindAssistenciaByCNPJ(cnpj As String) As Fornecedor
+        Dim conn As New Connection
+        Dim strSQL As New StringBuilder
+
+        strSQL.Append("SELECT * FROM Fornecedores ")
+        strSQL.Append("WHERE cnpj = @cnpj;")
+
+        conn.AddParameter("@cnpj", cnpj)
+
+        Dim dt As DataTable = conn.ExecuteSelect(strSQL.ToString)
+
+        Dim fornecedor As New Fornecedor()
+        fornecedor.CNPJ = CStr(dt.Rows(0).Item("cnpj"))
+        fornecedor.RazaoSocial = CStr(dt.Rows(0).Item("razaoSocial"))
+        fornecedor.NomeFantasia = CStr(dt.Rows(0).Item("nomeFantasia"))
+        fornecedor.Telefone = CStr(dt.Rows(0).Item("telefone"))
+        fornecedor.InicioRelacionamento = CDate(dt.Rows(0).Item("inicioRelacionamento"))
+        fornecedor.TipoFornecedor = CStr(dt.Rows(0).Item("tipoFornecedor"))
+        fornecedor.Logradouro = CStr(dt.Rows(0).Item("logradouro"))
+        fornecedor.Numero = CStr(dt.Rows(0).Item("numero"))
+        fornecedor.Bairro = CStr(dt.Rows(0).Item("bairro"))
+        fornecedor.Cidade = CStr(dt.Rows(0).Item("cidade"))
+        fornecedor.Estado = CStr(dt.Rows(0).Item("estado"))
+        fornecedor.CEP = CStr(dt.Rows(0).Item("cep"))
+        fornecedor.TipoEndereco = CStr(dt.Rows(0).Item("tipoEndereco"))
+        fornecedor.Assistencia = fornecedor
+
+        Return fornecedor
+
+    End Function
+
+
+End Class
---
+++ NobelTheater/DAOs/NotaFiscalVendaDAO.vb
@@ -0,0 +1,100 @@
+﻿Option Explicit On
+Option Strict On
+
+Imports System.Text
+
+Public Class NotaFiscalVendaDAO
+
+    'OK
+    Public Function Insert(ByVal notaFiscal As NotaFiscalVenda) As Boolean
+        Dim conn As New Connection
+        Dim strSQL As New StringBuilder
+
+        strSQL.Append("INSERT INTO NotasFiscaisVendas(statusNF, emissaoNF, numeroNF, dataAprovacao, dataFinalObra, idOrcamento) ")
+        strSQL.Append("VALUES(@status, @emissao, @numero, @dataAprovacao, @dataFinalObra, @idOrcamento);")
+
+        conn.AddParameter("@status", notaFiscal.Status)
+        conn.AddParameter("@emissao", notaFiscal.EmissaoNF)
+        conn.AddParameter("@numero", notaFiscal.NumeroNF)
+        conn.AddParameter("@dataAprovacao", notaFiscal.DataAprovacao)
+        conn.AddParameter("@dataFinalObra", notaFiscal.DataFinalObra)
+        conn.AddParameter("@idOrcamento", notaFiscal.Orcamento.ID)
+
+        Return conn.ExecuteCommand(strSQL.ToString)
+
+    End Function
+
+    'OK
+    Public Function UpdateStatus(ByVal notaFiscal As NotaFiscalVenda) As Boolean
+        Dim conn As New Connection
+        Dim strSQL As New StringBuilder
+
+        strSQL.Append("UPDATE NotasFiscaisVendas ")
+        strSQL.Append("SET status = @status ")
+        strSQL.Append("WHERE id = @id;")
+
+        conn.AddParameter("@status", notaFiscal.Status)
+        conn.AddParameter("@id", notaFiscal.ID)
+
+        Return conn.ExecuteCommand(strSQL.ToString)
+
+    End Function
+
+    'OK
+    Public Function FindAll() As List(Of NotaFiscalVenda)
+        Dim conn As New Connection
+        Dim dt As DataTable = conn.ExecuteSelect("SELECT * FROM NotasFiscaisVendas;")
+
+        Dim orcamentoDAO As New OrcamentoDAO()
+
+        Dim notasFiscais As New List(Of NotaFiscalVenda)
+
+        For Each row As DataRow In dt.Rows
+            Dim notaFiscal As New NotaFiscalCompra()
+            notaFiscal.ID = CLng(row.Item("id"))
+            notaFiscal.DataAprovacao = CDate(row.Item("dataAprovacao"))
+            notaFiscal.DataFinalObra = CDate(row.Item("dataFinalObra"))
+            notaFiscal.Status = CStr(row.Item("statusNF"))
+            notaFiscal.EmissaoNF = CDate(row.Item("emissaoNF"))
+            notaFiscal.NumeroNF = CStr(row.Item("numeroNF"))
+            notaFiscal.Orcamento = orcamentoDAO.FindByID(CLng(row.Item("idOrcamento")))
+            notasFiscais.Add(notaFiscal)
+        Next
+
+        Return notasFiscais
+
+    End Function
+
+    'OK
+    Public Function FindByStatus(status As String) As List(Of NotaFiscalCompra)
+
+        Dim conn As New Connection
+        Dim strSQL As New StringBuilder
+
+        strSQL.Append("SELECT * FROM NotasFiscaisCompras WHERE status = @status;")
+
+        conn.AddParameter("@status", status)
+
+        Dim dt As DataTable = conn.ExecuteSelect(strSQL.ToString)
+
+        Dim cotacaoDAO As New CotacaoDAO()
+
+        Dim notasFiscais As New List(Of NotaFiscalCompra)
+
+        For Each row As DataRow In dt.Rows
+            Dim notaFiscal As New NotaFiscalCompra()
+            notaFiscal.ID = CLng(row.Item("id"))
+            notaFiscal.DataAprovacao = CDate(row.Item("dataAprovacao"))
+            notaFiscal.Status = CStr(row.Item("statusNF"))
+            notaFiscal.EmissaoNF = CDate(row.Item("emissaoNF"))
+            notaFiscal.NumeroNF = CStr(row.Item("numeroNF"))
+            notaFiscal.Cotacao = cotacaoDAO.FindByID(CLng(row.Item("idCotacao")))
+            notasFiscais.Add(notaFiscal)
+        Next
+
+        Return notasFiscais
+
+    End Function
+
+End Class
+
